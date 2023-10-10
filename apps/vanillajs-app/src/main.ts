f6b36c5f00eede06cbd6246a62720fce102c6f9e
Ashish Patel <ashishp@vmware.com>
2023-10-09 17:03:25 -0700

    remove commented code

--- apps/vanillajs-app/src/main.ts
+++ apps/vanillajs-app/src/main.ts
@@ -2,7 +2,7 @@ import singleSpaHtml from 'single-spa-html';
 import template from './index.html';
 
 const htmlLifecycles = singleSpaHtml({
-  template: template, // '<div>Hello, Plain Vaniall Application!</div>',
+  template: template,
 });
 
 export const {
